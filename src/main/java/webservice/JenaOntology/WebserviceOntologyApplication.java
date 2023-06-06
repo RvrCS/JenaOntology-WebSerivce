5b707db379a2b2e33a33f9cfad3aae37674cd503
River <75063620+RvrCS@users.noreply.github.com>
2023-06-05 19:17:37 -0700

    Initial commit

---
+++ src/main/java/webservice/JenaOntology/WebserviceOntologyApplication.java
@@ -0,0 +1,13 @@
+package webservice.JenaOntology;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebserviceOntologyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebserviceOntologyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/webservice/JenaOntology/WebserviceOntologyApplicationTests.java
@@ -0,0 +1,13 @@
+package webservice.JenaOntology;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebserviceOntologyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
